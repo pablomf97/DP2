eb65b8b0ee992159e409cdb66f71623753474cf6
McAllisterDTur <Joseph.jmlc@gmail.com>
2019-02-26 12:11:52 +0100

    Se ha creado el registro de un member
    Se puede registrar, editar y ver un member
    Puede que al integrar, en el resto de proyectos falle, se debe a la falta del finder, una vez que esté implementado, se modificará el member para tener en cienta el finder y funcionará (lo he probado con un finder de mentira para probar que vaya)

--- Workspace/Sample-Project-1.19/src/main/java/services/MemberService.java
+++ Workspace/Sample-Project-1.19/src/main/java/services/MemberService.java
@@ -1,23 +1,22 @@
+
 package services;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 
 import repositories.MemberRepository;
 import security.Authority;
 import security.LoginService;
 import security.UserAccount;
-import domain.Actor;
-import domain.Finder;
 import domain.Member;
+import forms.MemberForm;
 
 @Service
 @Transactional
@@ -26,7 +25,14 @@ public class MemberService {
 	// Managed Repository
 
 	@Autowired
-	private MemberRepository memberRepository;
+	private MemberRepository	memberRepository;
+
+	@Autowired
+	private UserAccountService	userAccountService;
+
+	@Autowired
+	private Validator			validator;
+
 
 	// Supporting Services
 
@@ -35,9 +41,9 @@ public class MemberService {
 	/* Find one by ID */
 	public Member findOne(final int memberId) {
 		Member res;
-
+		Assert.isTrue(memberId != 0, "Wrong member id");
 		res = this.memberRepository.findOne(memberId);
-
+		Assert.notNull(res, "The member does not exist");
 		return res;
 
 	}
@@ -54,22 +60,20 @@ public class MemberService {
 
 	/* Create a member */
 	public Member create() {
-		Member res;
-		res = new Member();
+		final Member res = new Member();
+		final UserAccount a = this.userAccountService.create();
+		final Authority auth = new Authority();
+		auth.setAuthority(Authority.MEMBER);
+		a.addAuthority(auth);
+		a.setIsBanned(false);
+		res.setUserAccount(a);
 
-		Authority authority = new Authority();
-		authority.setAuthority(Authority.MEMBER);
+		return res;
+	}
 
-		List<Authority> authorities = new ArrayList<Authority>();
-		authorities.add(authority);
+	public MemberForm createForm() {
 
-		Finder finder = new Finder();
-		res.setFinder(finder);
-
-		UserAccount userAccount = new UserAccount();
-		userAccount.setAuthorities(authorities);
-		userAccount.setIsBanned(false);
-		res.setUserAccount(userAccount);
+		final MemberForm res = new MemberForm();
 
 		return res;
 	}
@@ -77,51 +81,81 @@ public class MemberService {
 	/* Save a member */
 	public Member save(final Member member) {
 		Member result;
-		Actor principal;
-
 		Assert.notNull(member);
-
-		// Assert.isTrue(this.utilityService.validEmail(admin.getEmail()),
-		// "administrator.email");
-
-		principal = this.findByPrincipal();
-		Assert.notNull(principal);
 		if (member.getId() == 0) {
-			Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
-			member.getUserAccount().setPassword(
-					passwordEncoder.encodePassword(member.getUserAccount()
-							.getPassword(), null));
+			final UserAccount account = member.getUserAccount();
+			final Authority au = new Authority();
+			au.setAuthority(Authority.MEMBER);
+			Assert.isTrue(account.getAuthorities().contains(au), "You can not register with this authority");
+			final UserAccount savedAccount = this.userAccountService.save(account);
+			member.setUserAccount(savedAccount);
+			result = this.memberRepository.save(member);
+			//TODO: cuando este el sistema de box, crear los iniciales
+			//TODO: crear el finder y ponerle el member nuevo
+			//this.boxService.initializeDefaultBoxes(result);
+			//this.finderService.initializeDefaultFinder(result);
+		} else {
+			final UserAccount userAccount = LoginService.getPrincipal();
+			final Member memberBD = this.memberRepository.findOne(member.getId());
+			Assert.isTrue(member.getUserAccount().equals(userAccount) && memberBD.getUserAccount().equals(userAccount), "This account does not belong to you");
+			result = this.memberRepository.save(member);
 		}
-
-		result = this.memberRepository.save(member);
-
 		return result;
 	}
 
-	// Other business methods
+	/**
+	 * Change the incomplete member to an domain object
+	 * 
+	 * @param member
+	 * @param binding
+	 * @return member
+	 */
+	public Member reconstruct(final MemberForm memberForm, final BindingResult binding) {
+		Member result = this.create();
+		if (memberForm.getId() == 0) {
+			result.getUserAccount().setUsername(memberForm.getUsername());
+			result.getUserAccount().setPassword(memberForm.getPassword());
+			result.setAddress(memberForm.getAddress());
+			result.setEmail(memberForm.getEmail());
+			result.setMiddleName(memberForm.getMiddleName());
+			result.setName(memberForm.getName());
+			result.setPhoneNumber(memberForm.getPhoneNumber());
+			result.setPhoto(memberForm.getPhoto());
+			result.setSurname(memberForm.getSurname());
+			this.validator.validate(memberForm, binding);
 
-	public Member findByPrincipal() {
-		Member res;
-		UserAccount userAccount;
-
-		userAccount = LoginService.getPrincipal();
-		Assert.notNull(userAccount);
-
-		res = this.findMemberByUserAccount(userAccount.getId());
-		Assert.notNull(res);
-
-		return res;
+		} else {
+			result = this.memberRepository.findOne(memberForm.getId());
+			if (this.checkValidation(memberForm, binding, result)) {
+				result.setAddress(memberForm.getAddress());
+				result.setEmail(memberForm.getEmail());
+				result.setMiddleName(memberForm.getMiddleName());
+				result.setName(memberForm.getName());
+				result.setPhoneNumber(memberForm.getPhoneNumber());
+				result.setPhoto(memberForm.getPhoto());
+				result.setSurname(memberForm.getSurname());
+			} else {
+				result = this.create();
+				result.setAddress(memberForm.getAddress());
+				result.setEmail(memberForm.getEmail());
+				result.setMiddleName(memberForm.getMiddleName());
+				result.setName(memberForm.getName());
+				result.setPhoneNumber(memberForm.getPhoneNumber());
+				result.setPhoto(memberForm.getPhoto());
+				result.setSurname(memberForm.getSurname());
+			}
+		}
+		return result;
 	}
-
-	public Member findMemberByUserAccount(final int userAccountId) {
-		Assert.isTrue(userAccountId != 0);
-
-		Member res;
-
-		res = this.memberRepository.findMemberByUserAccount(userAccountId);
-
-		Assert.notNull(res);
-
-		return res;
+	private boolean checkValidation(final MemberForm memberForm, final BindingResult binding, final Member member) {
+		boolean check = true;
+		memberForm.setCheckBox(true);
+		memberForm.setPassword(member.getUserAccount().getPassword());
+		memberForm.setPassword2(member.getUserAccount().getPassword());
+		memberForm.setUsername(member.getUserAccount().getUsername());
+		this.validator.validate(memberForm, binding);
+		if (binding.hasErrors())
+			check = false;
+		return check;
 	}
 }
---
+++ Workspace/Sample-Project-1.19/src/main/webapp/views/member/edit.jsp
@@ -0,0 +1,153 @@
+<%--
+ * action-1.jsp
+ *
+ * Copyright (C) 2019 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the 
+ * TDG Licence, a copy of which you may download from 
+ * http://www.tdg-seville.info/License.html
+ --%>
+
+<%@page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@taglib prefix="security"
+	uri="http://www.springframework.org/security/tags"%>
+<%@taglib prefix="display" uri="http://displaytag.sf.net"%>
+
+<!-- 	<p id="demo"></p>
+			document.getElementById("demo").innerHTML = pat; -->
+	
+<script>
+	function checkPhone(msg) {
+		var phone = document.getElementById("phoneNumber").value;
+		var phonePattern =  new RegExp(
+			/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$/);
+		var pat = phonePattern.test(phone);
+		if (pat) {
+			return true;
+		} else {
+			return confirm(msg);
+		}
+	}
+	function unCheck() {
+		var checkbox = document.getElementById('checkBox');
+		checkbox.checked = false;
+	}
+	window.onload = unCheck;
+</script>
+
+<p>
+	<spring:message code="actor.edit" />
+</p>
+
+<spring:message code="phone.confirmation" var="confirmTelephone" />
+<form:form action="member/edit.do" modelAttribute="memberForm"
+	methodParam="post" onsubmit="javascript: return checkPhone('${confirmTelephone}');">
+
+	<form:hidden path="id" />
+
+	<security:authorize access="isAnonymous()">
+
+		<form:label path="username">
+			<spring:message code="actor.userAccount.username" />:
+			</form:label>
+		<form:input path="username" />
+		<form:errors cssClass="error" path="username" />
+		<jstl:if test="${not empty uniqueUsername}">
+			<a class="error"><spring:message code="${uniqueUsername}" /></a>
+		</jstl:if>
+		<br />
+
+		<form:label path="password">
+			<spring:message code="actor.userAccount.password" />:
+			</form:label>
+		<form:password path="password" />
+		<form:errors cssClass="error" path="password" />
+		<jstl:if test="${not empty checkPass}">
+			<a class="error"><spring:message code="${checkPass}" /></a>
+		</jstl:if>
+		<br />
+
+		<form:label path="password2">
+			<spring:message code="actor.userAccount.password2" />:
+			</form:label>
+		<form:password path="password2" />
+		<form:errors cssClass="error" path="password2" />
+		<br />
+	</security:authorize>
+
+	<form:label path="surname">
+		<spring:message code="actor.surname" />:
+		</form:label>
+	<form:input path="surname" value="${member.surname}" />
+	<form:errors cssClass="error" path="surname" />
+	<br>
+
+	<form:label path="name">
+		<spring:message code="actor.name" />:
+		</form:label>
+	<form:input path="name" value="${member.name}" />
+	<form:errors cssClass="error" path="name" />
+	<br>
+
+	<form:label path="middleName">
+		<spring:message code="actor.middlename" />:
+		</form:label>
+	<form:input path="middleName" value="${member.middleName}" />
+	<form:errors cssClass="error" path="middleName" />
+	<br>
+
+	<form:label path="email">
+		<spring:message code="actor.email" />:
+		</form:label>
+	<form:input path="email" value="${member.email}" id="email" />
+	<form:errors cssClass="error" path="email" />
+	<jstl:if test="${not empty emailError}">
+		<a class="error"><spring:message code="${emailError}" /></a>
+	</jstl:if>
+	<br>
+
+	<form:label path="photo">
+		<spring:message code="actor.photo" />:
+		</form:label>
+	<form:input path="photo" value="${member.photo}" />
+	<form:errors cssClass="error" path="photo" />
+	<br>
+
+	<form:label path="phoneNumber">
+		<spring:message code="actor.phone" />:
+		</form:label>
+	<form:input path="phoneNumber" value="${member.phoneNumber}"
+		id="phoneNumber" />
+	<form:errors cssClass="error" path="phoneNumber" />
+	<br>
+
+	<form:label path="address">
+		<spring:message code="actor.address" />:
+		</form:label>
+	<form:input path="address" value="${member.address}" />
+	<form:errors cssClass="error" path="address" />
+	<br>
+	
+	<security:authorize access="isAnonymous()">
+		<form:label path="checkBox">
+			<spring:message code="actor.check.law" />:
+				</form:label>
+		<form:checkbox path="checkBox" id="checkBox" value="true" />
+		<jstl:if test="${not empty checkLaw}">
+			<a class="error"><spring:message code="${checkLaw}" /></a>
+		</jstl:if>
+		<br>
+	</security:authorize>
+
+	<input type="submit" name="save" id="save"
+		value='<spring:message code="actor.save"/>' />
+	<input type="button" name="cancel"
+		value="<spring:message code="actor.cancel" />"
+		onclick="javascript: relativeRedir('actor/display.do');" />
+	<br />
+</form:form>
