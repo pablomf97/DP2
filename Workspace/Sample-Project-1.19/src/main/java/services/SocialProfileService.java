c081ce92b43d43796c4cc66a3c0f5f350ab92650
jesusrico12 <jesus9610@gmail.com>
2019-03-02 20:38:53 +0100

    avances

--- Workspace/Sample-Project-1.19/src/main/java/services/SocialProfileService.java
+++ Workspace/Sample-Project-1.19/src/main/java/services/SocialProfileService.java
@@ -1,5 +1,6 @@
 package services;
 
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -12,87 +13,149 @@ import org.springframework.util.Assert;
 import repositories.SocialProfileRepository;
 
 import domain.Actor;
-import domain.Finder;
-import domain.Member;
-import domain.Procession;
+
 import domain.SocialProfile;
 
 @Service
 @Transactional
 public class SocialProfileService {
 
-	
+
 	// Managed repository ------------------------------
-		@Autowired
-		private SocialProfileRepository socialProfileRepository;
+	@Autowired
+	private SocialProfileRepository socialProfileRepository;
 
-		// Supporting services -----------------------
-		@Autowired
-		private ActorService actorService;
-		
-		
-		// Constructors
-		
-		public SocialProfileService() {
-			super();
-		}
-		
-		///CREATE
-		public SocialProfile create(){
-			SocialProfile result;
-			Actor principal;
+	// Supporting services -----------------------
+	@Autowired
+	private ActorService actorService;
 
-			principal=this.actorService.findByPrincipal();
-			Assert.notNull(principal);
 
-			result=new SocialProfile();
-			result.setActor(principal);
+	// Constructors
 
-			return result;
-		}
-		
-		///FINDONE
-		public SocialProfile findOne(final int socialProfileId) {
-			SocialProfile result;
-			Actor principal;
+	public SocialProfileService() {
+		super();
+	}
 
-			principal = this.actorService.findByPrincipal();
-			Assert.notNull(principal);
+	///CREATE
+	public SocialProfile create(){
+		SocialProfile result;
+		Actor principal;
 
-			result = this.socialProfileRepository.findOne(socialProfileId);
-			Assert.notNull(result);
+		principal=this.actorService.findByPrincipal();
+		Assert.notNull(principal);
 
-			return result;
-		}
+		result=new SocialProfile();
+		result.setActor(principal);
+
+		return result;
+	}
+
+	///FINDONE
+	public SocialProfile findOne(final int socialProfileId) {
+		SocialProfile result;
+		Actor principal;
+
+		principal = this.actorService.findByPrincipal();
+		Assert.notNull(principal);
+
+		result = this.socialProfileRepository.findOne(socialProfileId);
+		Assert.notNull(result);
+
+		return result;
+	}
 	////SAVE
 
-		public SocialProfile save(final SocialProfile socialProfile){
-			SocialProfile result;
-			Actor principal;
+	public SocialProfile save(final SocialProfile socialProfile){
+		SocialProfile result;
+		Actor principal;
 
 
-			Assert.notNull(socialProfile);
+		Assert.notNull(socialProfile);
 
-			principal=this.actorService.findByPrincipal();
-			Assert.notNull(principal);
-			
-			Assert.notNull(socialProfile.getName(), "socialProfile.NotEmpty");
-			Assert.notNull(socialProfile.getNick(), "socialProfile.NotEmpty");
-			Assert.notNull(socialProfile.getLinkProfile(), "socialProfile.NotEmpty");
-			result=this.socialProfileRepository.save(socialProfile);
-			Assert.notNull(result);
+		principal=this.actorService.findByPrincipal();
+		Assert.notNull(principal);
 
-			return result;
-
+		
+		if(socialProfile.getId() != 0){
+			Assert.isTrue(checkifPrincipalIsOwnerBySocialProfileId(socialProfile.getId()),"not.allowed");
 		}
-		//FINDALL
-		public Collection<SocialProfile> findAll() {
-			Collection<SocialProfile> result;
+		
+		
+		
+		
+		Assert.notNull(socialProfile.getName(), "socialProfile.NotEmpty");
+		Assert.notNull(socialProfile.getNick(), "socialProfile.NotEmpty");
+		Assert.notNull(socialProfile.getLinkProfile(), "socialProfile.NotEmpty");
+		result=this.socialProfileRepository.save(socialProfile);
+		Assert.notNull(result);
 
-			result = this.socialProfileRepository.findAll();
-			Assert.notNull(result);
+		return result;
 
-			return result;
+	}
+	//FINDALL
+	public Collection<SocialProfile> findAll() {
+		Collection<SocialProfile> result;
+
+		result = this.socialProfileRepository.findAll();
+		Assert.notNull(result);
+
+		return result;
+	}
+	//DELETE
+	
+	public void delete(final SocialProfile socialProfile) {
+		Actor principal;
+		
+
+		Assert.notNull(socialProfile);
+		Assert.isTrue(socialProfile.getId() != 0);
+		Assert.isTrue(checkifPrincipalIsOwnerBySocialProfileId(socialProfile.getId()),"not.allowed");
+		principal = this.actorService.findByPrincipal();
+		Assert.notNull(principal);
+
+
+		this.socialProfileRepository.delete(socialProfile);
+
+	}
+	
+	//// Other business methods
+	
+	public Boolean checkifPrincipalIsOwnerBySocialProfileId(int socialProfileID) {
+		Actor principal;
+		Boolean res = false;
+
+		principal = this.actorService.findByPrincipal();
+		
+		for(SocialProfile sp:this.findAll()){
+			if(socialProfileID==sp.getId()&&principal.getId()==sp.getActor().getId()){
+				res=true;
+			}
 		}
+		
+		return res;
+	}
+/*	public Collection<SocialProfile> socialProfilesbyActor(){
+		Collection<SocialProfile> cols;
+		Collection<SocialProfile> res=new ArrayList<SocialProfile>();
+		Actor principal;
 
+		principal = this.actorService.findByPrincipal();
+		
+		cols=this.findAll();
+		System.out.println("asd:"+cols);
+		for (SocialProfile sp:cols){
+			if(principal.getId()==sp.getActor().getId()){
+				res.add(sp);
+			}
+		}
+		return res;
+	}*/
+	
+	public Collection<SocialProfile> socialProfilesByUser(String username){
+		
+		Collection<SocialProfile> res;
+		res=this.socialProfileRepository.socialProfilesByUser(username);
+		return res;
+		
+	}
 }
