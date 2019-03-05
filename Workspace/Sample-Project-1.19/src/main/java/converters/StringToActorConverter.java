ab28cae089b97e354f629f485b0220e33398f28d
FraPazGal <frandepaz@yahoo.es>
2019-03-05 12:10:43 +0100

    Converters

---
+++ Workspace/Sample-Project-1.19/src/main/java/converters/StringToActorConverter.java
@@ -0,0 +1,39 @@
+
+package converters;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import repositories.ActorRepository;
+import domain.Actor;
+
+@Component
+@Transactional
+public class StringToActorConverter implements Converter<String, Actor> {
+
+	@Autowired
+	private ActorRepository	actorRepository;
+
+
+	@Override
+	public Actor convert(final String text) {
+		Actor result;
+		final int id;
+
+		try {
+			if (StringUtils.isEmpty(text))
+				result = null;
+			else {
+				id = Integer.valueOf(text);
+				result = this.actorRepository.findOne(id);
+			}
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+		return result;
+	}
+}
